b3ca8743a5541fb57309d1574ef99fe37839490a
kun99 <kuns.km@gmail.com>
2023-11-09 05:29:19 +0700

    removed port specification

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    port: 4000,
-  },
+  // server: {
+  //   port: 4000,
+  // },
 })
